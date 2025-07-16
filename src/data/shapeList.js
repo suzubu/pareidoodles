3158a0dbdc819f30f93c7c91f61278cd5fac46de
Suzy Burgum <suzyburgum@gmail.com>
2025-07-16 09:35:31 -0400

    Add new SVG shapes and update shape list
    - Added blue semi-circle shape (blue-semi.svg)
    - Added blue squiggle shape (blue-squiggle.svg)
    - Added green rectangle shape (green-rec.svg)
    - Added second green shape (green-shape2.svg)
    - Added green star shape (green-star.svg)
    - Added pink blob shape (pink-blob.svg)
    - Added pink semi-circle shape (pink-semi.svg)
    - Added purple blob shape (purple-blob.svg)
    - Added purple triangle shape (purple-triangle.svg)
    - Added red rectangle shape (red-rec.svg)
    - Added red triangles shape (red-triangles.svg)
    - Added small eye shape (small-eye.svg)
    - Added small eyes shape (small-eyes.svg)
    - Added wide eye shape (wide-eye.svg)
    - Added yellow semi-circle shape (yellow-semi.svg)
    - Added yellow triangle shape (yellow-triangle.svg)
    - Updated shapeList.js to include new shapes

--- src/data/shapeList.js
+++ src/data/shapeList.js
@@ -5,6 +5,24 @@ import { ReactComponent as BluePent } from "../assets/shapes/blue-pentagon.svg";
 import { ReactComponent as Green1 } from "../assets/shapes/green-shape1.svg";
 import { ReactComponent as PurpSq } from "../assets/shapes/purple-squiggle.svg";
 import { ReactComponent as YellowStar } from "../assets/shapes/yellow-star.svg";
+import { ReactComponent as BlueSemi } from "../assets/shapes/blue-semi.svg";
+import { ReactComponent as BlueSquiggle } from "../assets/shapes/blue-squiggle.svg";
+import { ReactComponent as GreenRec } from "../assets/shapes/green-rec.svg";
+import { ReactComponent as Green2 } from "../assets/shapes/green-shape2.svg";
+import { ReactComponent as GreenStar } from "../assets/shapes/green-star.svg";
+import { ReactComponent as PinkBlob } from "../assets/shapes/pink-blob.svg";
+import { ReactComponent as PinkSemi } from "../assets/shapes/pink-semi.svg";
+import { ReactComponent as PurpleBlob } from "../assets/shapes/purple-blob.svg";
+import { ReactComponent as PurpleTri } from "../assets/shapes/purple-triangle.svg";
+import { ReactComponent as RedRec } from "../assets/shapes/red-rec.svg";
+import { ReactComponent as RedTris } from "../assets/shapes/red-triangles.svg";
+import { ReactComponent as SmallEye } from "../assets/shapes/small-eye.svg";
+import { ReactComponent as SmallEyes } from "../assets/shapes/small-eyes.svg";
+import { ReactComponent as WideEye } from "../assets/shapes/wide-eye.svg";
+import { ReactComponent as YellowSemi } from "../assets/shapes/yellow-semi.svg";
+import { ReactComponent as YellowTri } from "../assets/shapes/yellow-triangle.svg";
+
+// import {ReactComponent as } from "../assets/shapes/"
 
 const padding = 50;
 const shapeSize = 100;
@@ -13,10 +31,27 @@ const shapeList = [
   { id: "pinkRec", Component: PinkRec },
   { id: "redCirc", Component: RedCirc },
   { id: "wideEyes", Component: WideEyes },
+  { id: "wideEye", Component: WideEye },
   { id: "bluePent", Component: BluePent },
   { id: "green1", Component: Green1 },
   { id: "purpSq", Component: PurpSq },
   { id: "yellowStar", Component: YellowStar },
+  { id: "blueSemi", Component: BlueSemi },
+  { id: "blueSquiggle", Component: BlueSquiggle },
+  { id: "greenRec", Component: GreenRec },
+  { id: "green2", Component: Green2 },
+  { id: "greenStar", Component: GreenStar },
+  { id: "pinkBlob", Component: PinkBlob },
+  { id: "pinkSemi", Component: PinkSemi },
+  { id: "purpleBlob", Component: PurpleBlob },
+  { id: "purpleTri", Component: PurpleTri },
+  { id: "redRec", Component: RedRec },
+  { id: "redTris", Component: RedTris },
+  { id: "smallEye", Component: SmallEye },
+  { id: "smallEyes", Component: SmallEyes },
+  { id: "yellowSemi", Component: YellowSemi },
+  { id: "yellowTri", Component: YellowTri },
+
   // add more shapes here…
 ].map((shape) => ({
   ...shape,
